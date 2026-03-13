b73f4823e78056eae01f11418c69e69fbefb8a12
Can Sarkuş <66849193+PengiSarkus@users.noreply.github.com>
2026-03-13 16:57:47 +0300

    Refactor array functions and update swap logic

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -8,27 +8,29 @@ void swapValues(int* p1,int*p2) {
 }
 void printArray(int* arr, int size) {
     for (int i = 0; i < size; i++) {
-        cout << *arr << endl;
+        cout << *arr << " ";
         arr++;
     }
 
 }
 int findMax(int* arr, int size) {
-    int max = arr[0];
+    int max = *arr;
     for (int i = 1; i < size; i++) {
-        if (arr[i] > max) {
-            max = arr[i];
+        if (*(arr +i) > max) {
+            max = *(arr+i);
         }
     }
     return max;
 }
 void reverseArray(int* arr, int size) {
-    for (int i = 0; i < size/2; i++) {
-        int temp = arr[i];
-        arr[i] = arr[size-1-i];
-        arr[size-1-i] = temp;
+    int* start = arr;
+    int* end = arr + size - 1;
+    while (start < end) {
+        swapValues(start, end);
+        start++;
+        end--;
     }
-}
+    }
 int* createArray(int size) {
     int* arr = new int[size];
     return arr;
@@ -53,12 +55,12 @@ int main() {
     cout << "Maximum element : " << findMax(arr, size) << endl;
     cout << "-------------------------------------";
     cout << endl;
-    cout << "Swapping two Arrays" << endl;
+    cout << "Swapping two Numbers" << endl;
     cout << "Before swapping" << endl;
     int a = 10;
     int b = 20;
     cout << "a = " << a << " " << "b = " << b << endl;
-    swapValues(arr, arr);
+    swapValues(&a, &b);
     cout << "After swapping" << endl;
     cout << "a = " << a << " " << "b = " << b << endl;
     cout << "-------------------------------------";
